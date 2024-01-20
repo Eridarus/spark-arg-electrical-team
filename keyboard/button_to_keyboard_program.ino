033f6b0412109f20a7d04085bae697fb30873da4
Hannalee Wu <hannaleehzw@gmail.com>
2024-01-20 11:54:26 -0500

    updated keyboard sketch

--- keyboard/button_to_keyboard_program.ino
+++ keyboard/button_to_keyboard_program.ino
@@ -1,20 +1,22 @@
 #include <Keyboard.h>
 
-const int buttonPin_up = 14;  // the number of the pushbutton pin
-int buttonState_up = 0;  // initial value for variable for reading the pushbutton status
-char upKey = 	KEY_UP_ARROW; // action key
+// constants - the number associated with the pushbutton pin
+const int buttonPin_up = 14;
+const int buttonPin_down = 15;
+const int buttonPin_left = 16;
+const int buttonPin_right = 17;
 
-const int buttonPin_down = 14;  // the number of the pushbutton pin
-int buttonState_down = 0;  // initial value for variable for reading the pushbutton status
-char upKey = 	KEY_UP_ARROW; // action key
+// initial value for variable for reading the pushbutton status
+int buttonState_up = 0;
+int buttonState_down = 0;
+int buttonState_left = 0;
+int buttonState_right = 0;  
 
-const int buttonPin_left = 14;  // the number of the pushbutton pin
-int buttonState_left = 0;  // initial value for variable for reading the pushbutton status
-char upKey = 	KEY_UP_ARROW; // action key
-
-const int buttonPin_right = 14;  // the number of the pushbutton pin
-int buttonState_right = 0;  // initial value for variable for reading the pushbutton status
-char upKey = 	KEY_UP_ARROW; // action key
+// action keys
+char up = 	KEY_UP_ARROW;
+char down = 	KEY_DOWN_ARROW;
+char left = 	KEY_LEFT_ARROW;
+char right = 	KEY_RIGHT_ARROW;
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,9 +27,25 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // read the state of the pushbutton value:
-  buttonState1 = digitalRead(buttonPin1);
-  if (buttonState1==HIGH) {
-    Keyboard.press(ctrlKey);
+  buttonState_up = digitalRead(buttonPin_up);
+  buttonState_down = digitalRead(buttonPin_down);
+  buttonState_left = digitalRead(buttonPin_left);
+  buttonState_right = digitalRead(buttonPin_right);
+  // press keys associated with respective buttons
+  if (buttonState_up==HIGH) {
+    Keyboard.press(up);
+    Keyboard.releaseAll();
+  }
+  if (buttonState_down==HIGH) {
+    Keyboard.press(down);
+    Keyboard.releaseAll();
+  }
+  if (buttonState_left==HIGH) {
+    Keyboard.press(left);
+    Keyboard.releaseAll();
+  }
+  if (buttonState_left==HIGH) {
+    Keyboard.press(left);
     Keyboard.releaseAll();
   }
 
